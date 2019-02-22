241d1ca3073d7d9632b1c27b95d743fbb2a39a09
karthikswarna <33567900+karthikswarna@users.noreply.github.com>
2019-02-22 16:52:23 +0530

    Update Q1_2.s

--- Q1_2.s
+++ Q1_2.s
@@ -9,11 +9,11 @@ main:
 	lw $t0, var
 	li $s0, 0				# register which indicates whether the number is positive or negative.
 
-	srl $t1, $t0, 31		# obtaining the MSB
+	srl $t1, $t0, 31			# obtaining the MSB
 	andi $t1, $t1, 1
 
-	beq $t1, $zero, positive # if MSB = 0, $s0 = 0
-	addi $s0, $s0, 1		 # if MSB = 1, $s0 = 1
+	beq $t1, $zero, positive 		# if MSB = 0, $s0 = 0
+	addi $s0, $s0, 1		 	# if MSB = 1, $s0 = 1
 positive:
 	jr $ra
 
